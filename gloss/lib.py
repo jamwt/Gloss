5dd6ace2921e304299109d5c9b0c07bb42159a41
Jamie Turner <jamie@bu.mp>
2010-10-29 12:33:18 -0700

    Add support for greenlets.

--- gloss/lib.py
+++ gloss/lib.py
@@ -7,11 +7,22 @@ from collections import defaultdict
 
 current_catalog = None
 default_locale = None
+get_thread_id = None
 thread_contexts = defaultdict(list)
 
-def load_gloss_catalog(d, def_locale='en_US'):
+def load_gloss_catalog(d, def_locale='en_US', use_greenlet=False):
     global default_locale 
     global current_catalog
+    global get_thread_id
+
+    if use_greenlet:
+        import greenlet
+        def greenlet_get_thread():
+            return id(greenlet.getcurrent())
+        get_thread_id = greenlet_get_thread
+    else:
+        get_thread_id = get_ident
+
     current_catalog = defaultdict(dict)
     for f in get_x_txt_files(d):
         locale_n = locale_from_fn(f)
@@ -22,7 +33,7 @@ def load_gloss_catalog(d, def_locale='en_US'):
 
 def get_translation(s):
     assert current_catalog, "No catalog loaded; use load() first"
-    tid = get_ident()
+    tid = get_thread_id()
     if thread_contexts[tid]:
         thread_cat = current_catalog[thread_contexts[tid][-1]]
         if thread_cat.get(s):
@@ -34,7 +45,7 @@ def get_translation(s):
 @contextmanager
 def lang(l):
     assert current_catalog, "No catalog loaded; use load() first"
-    tid = get_ident()
+    tid = get_thread_id()
     thread_contexts[tid].append(l)
     yield
     thread_contexts[tid].pop()
